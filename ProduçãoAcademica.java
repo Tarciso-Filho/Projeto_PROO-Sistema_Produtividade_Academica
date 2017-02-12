0a370bb8122f7dd5ba2974846b1fe0c3296f1254
Tarciso-Filho <tarciso.filho@hotmail.com>
2017-02-12 15:46:53 -0300

    Criação da Classe Produção Academica
    Issue 1.6

--- ProduçãoAcademica.java
+++ ProduçãoAcademica.java
@@ -1,6 +1,50 @@
 package gestãodeproduçãoacademica;
+import java.util.ArrayList;
 
 public class ProduçãoAcademica
 {
+    private String titulo;
+    private ArrayList<Colaborador> autores;
+    private Projeto associado;
+    
+    public ProduçãoAcademica(String novoTitulo, Projeto novoAssociado)
+    {
+        this.titulo = novoTitulo;
+        this.associado = novoAssociado;
+    }
+    
+    public void inicializarAutores()
+    {
+        this.autores = new ArrayList<Colaborador>();
+    }
+
+    public String getTitulo()
+    {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo)
+    {
+        this.titulo = titulo;
+    }
+
+    public ArrayList<Colaborador> getAutores()
+    {
+        return autores;
+    }
+
+    public void addAutores(Colaborador autor)
+    {
+        this.autores.add(autor);
+    }
+
+    public Projeto getAssociado() {
+        return associado;
+    }
+
+    public void setAssociado(Projeto associado) {
+        this.associado = associado;
+    }
+    
     
 }
